8d10cd5f7147372e4d2d86281d2ce87671133e37
Guilherme Falcão <guifalcao2010@hotmail.com>
2023-03-23 13:46:15 -0300

    project: configures project

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
+  apiKey: "AIzaSyAPuRiiBvahVXteeLbuYmrfWf7UfpUReUU",
+  authDomain: "portfolio-5430b.firebaseapp.com",
+  projectId: "portfolio-5430b",
+  storageBucket: "portfolio-5430b.appspot.com",
+  messagingSenderId: 829946024154,
+  appId: "1:829946024154:web:1c982443769d8e4d7315e3",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
